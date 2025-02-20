afff85b3e1f79b542b23ed3bbe8ba8a47cdcde5b
andriireunov <andrii.reunov@happentechnologies.com>
2025-02-20 19:10:11 +0200

    added check if ACF class exists

--- includes/register-json-blocks.php
+++ includes/register-json-blocks.php
@@ -1,43 +1,46 @@
 <?php
 
-add_filter( 'block_categories_all', 'exf_category_register', 10, 2 );
-add_action( 'init', 'exf_register_acf_blocks' );
+if(class_exists('ACF')) {
 
-function exf_register_acf_blocks(){
-	$pathes = glob( get_stylesheet_directory() . '/blocks/' . '*', GLOB_ONLYDIR );
-	foreach ( $pathes as $path ) {
-		$dirname = basename( $path );
-		register_block_type( __DIR__ . '/../blocks/'. $dirname );
+	add_filter( 'block_categories_all', 'exf_category_register', 10, 2 );
+	add_action( 'init', 'exf_register_acf_blocks' );
 
-		$props = [
-			'key' => "exf_block_{$dirname}",
-			'title' => "Block: {$dirname}"
-		];
-		$location = [
-			'location' => [
-				[
+	function exf_register_acf_blocks() {
+		$pathes = glob( get_stylesheet_directory() . '/blocks/' . '*', GLOB_ONLYDIR );
+		foreach ( $pathes as $path ) {
+			$dirname = basename( $path );
+			register_block_type( __DIR__ . '/../blocks/' . $dirname );
+
+			$props    = [
+				'key'   => "exf_block_{$dirname}",
+				'title' => "Block: {$dirname}"
+			];
+			$location = [
+				'location' => [
 					[
-						'param'     => 'block',
-						'operator'  => '==',
-						'value'     => "acf/{$dirname}",
+						[
+							'param'    => 'block',
+							'operator' => '==',
+							'value'    => "acf/{$dirname}",
+						],
 					],
 				],
-			],
-		];
-		$fields = require(__DIR__ . '/../blocks/'. $dirname . '/fields.php');
-		
-		acf_add_local_field_group(array_merge( $props, $fields, $location));
-	}
-}
+			];
+			$fields   = require( __DIR__ . '/../blocks/' . $dirname . '/fields.php' );
 
-function exf_category_register( $categories ) {
-	return array_merge(
-		array(
+			acf_add_local_field_group( array_merge( $props, $fields, $location ) );
+		}
+	}
+
+	function exf_category_register( $categories ) {
+		return array_merge(
 			array(
-				'slug'  => 'exf',
-				'title' => 'ExF'
+				array(
+					'slug'  => 'exf',
+					'title' => 'ExF'
+				),
 			),
-		),
-		$categories
-	);
+			$categories
+		);
+	}
 }
